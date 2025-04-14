59d609acf015d8b4d3b575e07d3ea8fb312c7793
Isabella-GOA <piconeisabella@savcds.org>
2025-04-14 16:30:11 -0700

    current look of the toy, only missing enemy and finished credits

--- toy-5/main.gd
+++ toy-5/main.gd
@@ -3,7 +3,11 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+<<<<<<< Updated upstream
 	pass # Replace with function body.
+=======
+	pass
+>>>>>>> Stashed changes
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,8 +19,34 @@ func _on_player_dead() -> void:
 	$background.stop()
 	$gameover.play(0)
 	$Timer.start()
+<<<<<<< Updated upstream
+=======
+	$ColorRect.show()
+	$ColorRect4.show()
+>>>>>>> Stashed changes
 
 
 func _on_timer_timeout() -> void:
 	$gameover.play(0)
 	$Timer.start()
+<<<<<<< Updated upstream
+=======
+
+
+func _on_button_pressed() -> void:
+	get_tree().reload_current_scene()
+
+
+func _on_start_pressed() -> void:
+	$ColorRect3.hide()
+
+func _on_button_2_pressed() -> void:
+	$ColorRect3/menu.hide()
+	$ColorRect3/credits.show()
+	$ColorRect3/return.show()
+
+func _on_return_pressed() -> void:
+	$ColorRect3/menu.show()
+	$ColorRect3/credits.hide()
+	$ColorRect3/return.hide()
+>>>>>>> Stashed changes
--- toy-5/player.gd
+++ toy-5/player.gd
@@ -6,11 +6,23 @@ const JUMP_VELOCITY = -400.0
 var damage = 0
 signal dead
 signal hit
+<<<<<<< Updated upstream
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("attack"):
 			emit_signal("hit")
 			print('hit')
+=======
+var enemy = false
+
+func _physics_process(delta: float) -> void:
+	if enemy == true:
+		if Input.is_action_just_pressed("attack"):
+				emit_signal("hit")
+				$Timer.start()
+				$AnimatedSprite2D.play("attack")
+				$AnimatedSprite2D2.hide()
+>>>>>>> Stashed changes
 
 	if HEALTH > 0:
 		var direction := Input.get_axis("ui_left", "ui_right")
@@ -32,7 +44,11 @@ func _physics_process(delta: float) -> void:
 			$AnimatedSprite2D.play("up")
 		if velocity.y > 0:
 			$AnimatedSprite2D.play("down")
+<<<<<<< Updated upstream
 		if velocity.x == 0 and velocity.y == 0:
+=======
+		if velocity.x == 0 and velocity.y == 0 and enemy == false:
+>>>>>>> Stashed changes
 			$AnimatedSprite2D.play("idle")
 	else:
 		pass
@@ -45,11 +61,15 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 		$Label.text = "Health: %d" % HEALTH
 	if HEALTH == 0:
 		emit_signal("dead")
+<<<<<<< Updated upstream
 		print("Game Over")
+=======
+>>>>>>> Stashed changes
 		$AnimatedSprite2D.play("dead")
 
 
 func _on_area_2d_2_body_entered(body: Node2D) -> void:
+<<<<<<< Updated upstream
 	if body.name != "enemystandin":
 		print(body.name)
 	if body.name == "enemystandin":
@@ -58,12 +78,20 @@ func _on_area_2d_2_body_entered(body: Node2D) -> void:
 			print('hit')
 			$Timer.start()
 			$AnimatedSprite2D.play("attack")
+=======
+	if body.name == "enemystandin":
+		enemy = true
+		$AnimatedSprite2D2.show()
+>>>>>>> Stashed changes
 
 
 func pond(body):
 	if body.name == "player":
 		emit_signal("dead")
+<<<<<<< Updated upstream
 		print("Game Over")
+=======
+>>>>>>> Stashed changes
 		HEALTH = 0
 		$Label.text = "Health: %d" % HEALTH
 		$AnimatedSprite2D.play("dead")
@@ -71,4 +99,15 @@ func pond(body):
 
 func _on_timer_timeout() -> void:
 	$AnimatedSprite2D.play("idle")
+<<<<<<< Updated upstream
 	print('done')
+=======
+	$Timer.stop()
+	if enemy == true:
+		$AnimatedSprite2D2.show()
+
+func _on_area_2d_2_body_exited(body: Node2D) -> void:
+	if body.name == "enemystandin":
+		enemy = false
+		$AnimatedSprite2D2.hide()
+>>>>>>> Stashed changes
